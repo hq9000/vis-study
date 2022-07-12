899ff01e63e3f8cb9a46d5536176f7aeba00ae93
Sergey Grechin <grechin.sergey@gmail.com>
2022-07-12 11:11:40 +0300

    colorisation

--- vis-study/spec-generation.py
+++ vis-study/spec-generation.py
@@ -45,8 +45,9 @@ def generate_vega_spec(request: GenerationRequest) -> Dict:
     """
 
     data_name = "table"
-    xscale_name = "xscale"
-    yscale_name = "yscale"
+    x_scale_name = "xscale"
+    y_scale_name = "yscale"
+    color_scale_name = "color"
 
     res: Dict = {
         "$schema": "https://vega.github.io/schema/vega/v5.json",
@@ -55,39 +56,35 @@ def generate_vega_spec(request: GenerationRequest) -> Dict:
         "padding": 5,
         "data": [{
             "name": data_name,
-            "values": [
-                {"x": 0.05, "y": 0.09, "c": 0.98},
-                {"x": 0.04, "y": 0.12, "c": 0.23},
-                {"x": 0.01, "y": 0.01, "c": 0.23}
-            ]
+            "url": generate_relative_path_to_data(request),
+            "format": {"type": request.data_format.value, "parse": "auto"}
         }
-
-            # {
-            #     "name": "airports",
-            #     "url": "data/airports.csv",
-            #     "format": {"type": "csv", "parse": "auto"}
-            # }
-
         ],
         "scales": [
             {
-                "name": xscale_name,
+                "name": x_scale_name,
                 "type": "linear",
                 "domain": {"data": data_name, "field": FIELD_X_NAME},
                 "range": "width",
                 "padding": 0.05,
             },
             {
-                "name": yscale_name,
+                "name": y_scale_name,
                 "type": "linear",
                 "domain": {"data": data_name, "field": FIELD_Y_NAME},
                 "nice": True,
                 "range": "height"
+            },
+            {
+                "name": color_scale_name,
+                "type": "ordinal",
+                "range": {"scheme": "category10"},
+                "domain": {"data": data_name, "field": FIELD_CATEGORY_NAME}
             }
         ],
         "axes": [
-            {"orient": "bottom", "scale": xscale_name, "grid": True},
-            {"orient": "left", "scale": yscale_name, "grid": True}
+            {"orient": "bottom", "scale": x_scale_name, "grid": True},
+            {"orient": "left", "scale": y_scale_name, "grid": True}
         ],
         "marks": [
             {
@@ -96,13 +93,13 @@ def generate_vega_spec(request: GenerationRequest) -> Dict:
                 "from": {"data": data_name},
                 "encode": {
                     "update": {
-                        "x": {"scale": xscale_name, "field": FIELD_X_NAME},
-                        "y": {"scale": yscale_name, "field": FIELD_Y_NAME},
+                        "x": {"scale": x_scale_name, "field": FIELD_X_NAME},
+                        "y": {"scale": y_scale_name, "field": FIELD_Y_NAME},
 
                         "shape": {"value": "circle"},
                         "strokeWidth": {"value": 2},
                         "opacity": {"value": 0.5},
-                        "stroke": {"value": "#4682b4"},
+                        "stroke": {"value": "#FF0000", "scale": color_scale_name, "field": FIELD_CATEGORY_NAME},
                         "fill": {"value": "transparent"}
                     }
                 },
@@ -116,13 +113,13 @@ def generate_vega_spec(request: GenerationRequest) -> Dict:
 def save_vega_spec(request: GenerationRequest, vega_spec: Dict):
     json_string = json.dumps(vega_spec, indent=2)
 
-    path = Path(__file__).parent.resolve() / '..' / GENERATED_DIR_NAME / SPECS_DIR_NAME / (
-            request.experiment_name + '.json')
+    path = Path(__file__).parent.resolve() / '..' / GENERATED_DIR_NAME / generate_relative_path_to_spec(request)
     with open(path, "w") as file:
         file.write(json_string)
 
 
 def generate_data(request: GenerationRequest) -> None:
+    # noinspection PyUnusedLocal
     def generate_one_row(request: GenerationRequest, i: int) -> Dict:
 
         category = 'category_' + str(random.randint(0, request.num_categories))
@@ -146,8 +143,7 @@ def generate_data(request: GenerationRequest) -> None:
     out_file_name = (Path(__file__).parent.resolve()
                      / '..'
                      / GENERATED_DIR_NAME
-                     / DATA_DIR_NAME
-                     / (request.experiment_name + '.' + request.data_format.value))
+                     / generate_relative_path_to_data(request))
 
     if request.data_format == DataFormat.CSV:
         with open(out_file_name, "w") as f:
@@ -157,9 +153,9 @@ def generate_data(request: GenerationRequest) -> None:
         for row in generate_rows(request):
             writer.writerow(row)
     elif request.data_format == DataFormat.JSON:
+        rows = list(generate_rows(request))
         with open(out_file_name, "w") as f:
-            for row in generate_rows(request):
-                print(json.dumps(row), file=f)
+            f.write(json.dumps(rows))
     else:
         assert False, "unknown data format " + request.data_format.value
 
@@ -171,11 +167,27 @@ def generate_html(request: GenerationRequest) -> str:
     )
 
     template = env.get_template('index.html')
-    return template.render(request=request)
+    return template.render(
+        request=request,
+        path_to_data=generate_relative_path_to_data(request),
+        path_to_spec=generate_relative_path_to_spec(request)
+    )
+
+
+def generate_relative_path_to_html(request: GenerationRequest) -> str:
+    return request.experiment_name + ".html"
+
+
+def generate_relative_path_to_data(request: GenerationRequest) -> str:
+    return DATA_DIR_NAME + "/" + request.experiment_name + '_data.json'
+
+
+def generate_relative_path_to_spec(request: GenerationRequest) -> str:
+    return SPECS_DIR_NAME + "/" + request.experiment_name + '_spec.json'
 
 
 def save_html(request: GenerationRequest, html: str) -> None:
-    path = Path(__file__).parent.resolve() / '..' / GENERATED_DIR_NAME / (request.experiment_name + '.html')
+    path = Path(__file__).parent.resolve() / '..' / GENERATED_DIR_NAME / generate_relative_path_to_html(request)
     with open(path, "w") as file:
         file.write(html)
 
@@ -194,7 +206,7 @@ if __name__ == "__main__":
     request = GenerationRequest(
         experiment_name="exp1",
         num_points=100,
-        num_categories=40,
+        num_categories=4,
         width=500,
         height=500,
         data_format=DataFormat.JSON,
